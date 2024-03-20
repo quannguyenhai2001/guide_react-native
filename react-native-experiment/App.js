1e75c28cb3c771b6b0344a7143ee9a83abc6ca3d
quannguyenhai2001 <nguyenhaiquan.0121@gmail.com>
2024-03-20 11:28:04 +0700

    feat: add code

--- react-native-experiment/App.js
+++ react-native-experiment/App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 60,
+    paddingHorizontal: 10,
+    flex: 1
   },
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 40,
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    flex: 1
+  },
+  listContainer: {
+    borderTopWidth: 1,
+    flex: 3
+  },
+  note: {
+    marginBottom: 30,
+    backgroundColor: '#eee',
+  }
 });
+
+export default function App() {
+  const [value, setValue] = useState('');
+  const [notes, setNotes] = useState([]);
+  const handleOnChangeText = (value) => {
+    setValue(value);
+  }
+  const handleOnPressButton = () => {
+    setNotes((prevNotes) => [...prevNotes, value]);
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput onChangeText={handleOnChangeText} style={styles.input} placeholder='Add your note' />
+        <Button onPress={handleOnPressButton} color={'#000'} title="Add note" />
+      </View>
+      <View style={styles.listContainer}>
+        <ScrollView>
+          {notes.map((note, index) => (
+            <Text style={styles.note} key={index}>{note}</Text>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
